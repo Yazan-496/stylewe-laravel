bb11a8ac67abfe21bc722f16006824d0393ae233
Yazan <yazan.adnof@ramaaz.com>
2023-06-02 20:56:18 +0300

    - config vercel

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+//use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +19,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+
+    return 'Not adualt';
+}) -> name('not.adult');
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware("auth");
